6aa9f988c1598b700d4ddc6bdf8785178aa1ad7e
Arthur <arthur.gdd2005@gmail.com>
2026-07-01 09:42:47 -0300

    Web Cozinha versão 1.1

--- src/main/java/com/example/primeiroAppSpring/controller/UsuarioController.java
+++ src/main/java/com/example/primeiroAppSpring/controller/UsuarioController.java
@@ -1,5 +1,8 @@
 package com.example.primeiroAppSpring.controller;
 
+import com.example.primeiroAppSpring.model.Usuario;
+import com.example.primeiroAppSpring.service.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +13,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/")
 public class UsuarioController {
+    @Autowired
+    private final UsuarioService usuarioService;
+
+    public UsuarioController(UsuarioService usuarioService) {
+        this.usuarioService = usuarioService;
+    }
+
     @GetMapping("/cadastro")
     public String exibirCadastro(Model model){
         //Criando formulário vazio
@@ -22,12 +32,13 @@ public class UsuarioController {
     }
     @PostMapping("/cadastro")
     public String processarCadastro(@ModelAttribute UsuarioForm form, Model model){
-        if(!form.getSenha().equals(form.getConfirmarSenha())){
-            model.addAttribute("erro", "Erro, as senhas não correspondem");
+        String erro = usuarioService.cadastrar(form);
+
+        if (erro != null) {
+            model.addAttribute("erro", erro);
+            model.addAttribute("usuarioForm", form);
             return "cadastro";
         }
-        IO.println("Usuário criado com sucesso!");
-        IO.println(form.getNome()+" "+form.getEmail());
 
         return "redirect:/login";
     }
@@ -44,11 +55,13 @@ public class UsuarioController {
     }
     @PostMapping("/login")
     public String processarLogin(@ModelAttribute UsuarioForm form, Model model){
-        if(form.getEmail().endsWith("@df.senac.br")){
-            return "redirect:/";
+        Usuario usuario = usuarioService.autenticar(form.getEmail(), form.getSenha());
+        if(usuario == null){
+            model.addAttribute("erro","E-mail ou senha incorretos!");
+            return "login";
+
         }
-        model.addAttribute("erro", "E-mail ou senha inválidos");
-        return "login";
+        return "redirect:/";
     }
 
     @GetMapping("/alterarSenha")
--- src/main/java/com/example/primeiroAppSpring/service/UsuarioService.java
+++ src/main/java/com/example/primeiroAppSpring/service/UsuarioService.java
@@ -1,4 +1,56 @@
 package com.example.primeiroAppSpring.service;
 
+import com.example.primeiroAppSpring.controller.UsuarioForm;
+import com.example.primeiroAppSpring.model.Usuario;
+import com.example.primeiroAppSpring.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
 public class UsuarioService {
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    private BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
+    public String cadastrar(UsuarioForm form){
+        if(!form.getSenha().equals(form.getConfirmarSenha())){
+            return "As senhas não conferem";
+        }
+        IO.println(form.getEmail()+"esse é o email");
+        if(usuarioRepository.existsByEmail(form.getEmail())){
+            return "Email já cadastrado no banco";
+        }
+
+        if(!form.getEmail().endsWith("@df.senac.br")){
+            return "E-mail não valido. Insira um e-amil @df.senac.br";
+        }
+
+        String senhaCriptografada = encoder.encode(form.getSenha());
+
+        Usuario novoUsuario = new Usuario(form.getNome(), form.getEmail(),senhaCriptografada);
+
+        usuarioRepository.save(novoUsuario);
+
+        return null;
+    }
+
+    public Usuario autenticar (String email, String senha){
+
+        Optional<Usuario> resultado = usuarioRepository.findByEmail(email);
+        if (resultado.isEmpty()){
+            return null;
+        }
+
+        Usuario usuario = resultado.get();
+
+        if(!encoder.matches(senha, usuario.getSenha())){
+           return null;
+        }
+
+        return usuario;
+    }
 }
